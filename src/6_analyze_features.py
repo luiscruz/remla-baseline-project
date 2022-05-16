6ca40de967d86be55680ec94e378b81a37f013ae
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-05-16 14:41:02 +0200

    Full pipeline implemented

--- src/6_analyze_features.py
+++ src/6_analyze_features.py
@@ -1,13 +1,24 @@
 from joblib import dump, load
+import os
+import json
+
+RESULTS_DIRECTION = "results/popular_words.json"
 
 
 def get_list_words(tuple_list):
-
     words = list()
-
     for elem in tuple_list:
         words.append(elem[0])
     return words
+
+
+def dump_feat_results(results):
+
+    if os.path.exists(RESULTS_DIRECTION):
+        creation_time = int(os.path.getctime(RESULTS_DIRECTION)*10e5)
+        os.rename(RESULTS_DIRECTION, RESULTS_DIRECTION.replace("popular_words.", f"popular_words-{creation_time}.json"))
+    with open(f"{RESULTS_DIRECTION}", "w") as file_write:
+        file_write.write(json.dumps(results, indent=4))
 
 
 def print_words_for_tag(classifier, tag, tags_classes, index_to_words, all_words):
@@ -32,6 +43,8 @@ def print_words_for_tag(classifier, tag, tags_classes, index_to_words, all_words
 
     print('Top 5 most seen words words:\t{}'.format(', '.join(most_seen)))
     print('Top 5 least seen word words:\t{}\n'.format(', '.join(less_seen)))
+
+    dump_feat_results({"most_seen": most_seen, "least_seen": less_seen})
 
 
 mlb = load("output/multi_label_binarizer.joblib")
