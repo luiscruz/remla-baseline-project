d33ca9262584f8f86e70b3ba28a10359a168a40f
Nikos Gavalas <gavalnick@gmail.com>
2022-05-19 01:31:15 +0200

    Added functionality to export and load the model

---
+++ src/infer.py
@@ -0,0 +1,14 @@
+from src.text_preprocessing import process_for_inference
+
+
+def infer(data, classifier_mybag, classifier_tfidf, mlb, tfidf_vectorizer, words_to_index, dict_size):
+    X_mybag, X_tfidf = process_for_inference(data, words_to_index, dict_size, tfidf_vectorizer)
+
+    y_mybag_enc = classifier_mybag.predict(X_mybag)
+    y_tfidf_enc = classifier_tfidf.predict(X_tfidf)
+
+    y_mybag = mlb.inverse_transform(y_mybag_enc)
+    y_tfidf = mlb.inverse_transform(y_tfidf_enc)
+    # print(y_mybag)
+    # print(y_tfidf)
+    return y_mybag, y_tfidf
--- src/model.py
+++ src/model.py
@@ -1,7 +1,8 @@
 from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
-from text_preprocessing import get_train_test_data
+
+from src.text_preprocessing import get_train_test_data
 
 def train_classifier(X_train, y_train, penalty='l1', C=1):
     """
@@ -19,13 +20,12 @@ def train_classifier(X_train, y_train, penalty='l1', C=1):
     return clf
 
 def get_classifiers():
-    X_train, y_train, X_val, y_val, X_test, X_train_mybag, X_val_mybag, X_test_mybag, X_train_tfidf, X_val_tfidf, X_test_tfidf, tfidf_vocab, tags_counts = get_train_test_data(data=3)
+    X_train, y_train, X_val, y_val, X_test, X_train_mybag, X_val_mybag, X_test_mybag, X_train_tfidf, X_val_tfidf, X_test_tfidf, tfidf_vectorizer, tags_counts, words_to_index, dict_size = get_train_test_data(data=3)
     from sklearn.preprocessing import MultiLabelBinarizer
-    # %%
     mlb = MultiLabelBinarizer(classes=sorted(tags_counts.keys()))
     y_train = mlb.fit_transform(y_train)
     y_val = mlb.fit_transform(y_val)
     classifier_mybag = train_classifier(X_train_mybag, y_train)
     classifier_tfidf = train_classifier(X_train_tfidf, y_train)
 
-    return classifier_mybag, classifier_tfidf, y_train, y_val
+    return classifier_mybag, classifier_tfidf, y_train, y_val, mlb, tfidf_vectorizer, words_to_index, dict_size
