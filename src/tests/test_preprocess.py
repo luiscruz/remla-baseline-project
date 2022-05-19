36a7d8fb803a8ec7df9970cb88683ba36645a421
AbeleMM <AbeleMM@users.noreply.github.com>
2022-05-19 17:11:37 +0200

    VSC linting: disable pydocstyle in tests, have refactors show as warning

--- src/tests/test_preprocess.py
+++ src/tests/test_preprocess.py
@@ -1,14 +1,7 @@
-"""TODO Summary."""
-
 from src.preprocess import text_prepare
 
 
 def test_text_prepare():
-    """
-    TODO Summary.
-
-    TODO Description
-    """
     examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
                 "How to free c++ memory vector<int> * arr?"]
     answers = ["sql server equivalent excels choose function",
