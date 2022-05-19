e9bfa2fdcfdd01969a603c706f877318bc2518ee
GulikM <matsvandergulik@gmail.com>
2022-05-19 08:47:44 +0200

    store mlb model for reuse

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -14,7 +14,7 @@ def train_classifier(X_train, y_train, penalty='l1', C=1):
       return: trained classifier
     """
     
-    # Create and fit LogisticRegression wraped into OneVsRestClassifier.
+    # Create and fit LogisticRegression wrapped into OneVsRestClassifier.
     
     clf = LogisticRegression(penalty=penalty, C=C, dual=False, solver='liblinear')
     clf = OneVsRestClassifier(clf)
@@ -38,8 +38,13 @@ if __name__ == '__main__':
 				tags_counts[tag] = 1
 
 	mlb = MultiLabelBinarizer(classes=sorted(tags_counts.keys()))
+
 	y_train = mlb.fit_transform(y_train)
-	
+
+	# Store for reuse (at prediction and validation stages)
+	with open(ROOT_DIR / 'models/mlb.pkl', 'wb') as f:
+		pickle.dump(mlb, f)
+
 	classifier_tfidf = train_classifier(X_train_tfidf, y_train)
 	
 	with open(ROOT_DIR / 'models/tfidf.pkl', 'wb') as f:
