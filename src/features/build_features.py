fcb93e10490c2a6d372155e86ae241172324951b
moudemans <melchior.oudemans@gmail.com>
2022-05-23 09:28:17 +0200

    Finishes writing pre-processed data to file

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -5,17 +5,7 @@ import pandas as pd
 from scipy import sparse as sp_sparse
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from src.util.util import read_data
-
-# Dictionary of all tags from train corpus with their counts.
-tags_counts = {}
-# Dictionary of all words from train corpus with their counts.
-words_counts = {}
-
-DICT_SIZE = 5000
-INDEX_TO_WORDS = sorted(words_counts, key=words_counts.get, reverse=True)[:DICT_SIZE]
-WORDS_TO_INDEX = {word: i for i, word in enumerate(INDEX_TO_WORDS)}
-ALL_WORDS = WORDS_TO_INDEX.keys()
+from src.util.util import read_data, write_data
 
 
 def tfidf_features(X_train, X_val, X_test):
@@ -35,6 +25,7 @@ def tfidf_features(X_train, X_val, X_test):
     X_test = tfidf_vectorizer.transform(X_test)
 
     return X_train, X_val, X_test, tfidf_vectorizer.vocabulary_
+
 
 def my_bag_of_words(text, words_to_index, dict_size):
     """
@@ -75,29 +66,14 @@ def count_words_lists(list_of_lists):
     return count_dict
 
 
-def main(input_filepath='../../data/interim/', output_filepath='../../data/processed/'):
-    """ Runs data processing scripts to turn pre-processed data from (../interim) into
-        feature data ready to be trained/tested (saved in ../processed).
-    """
-    logger = logging.getLogger(__name__)
-    logger.info('making final data set from raw data')
-
-    train_file_name = 'train.tsv'
-    validation_file_name = 'validation.tsv'
-    test_file_name = 'test.tsv'
-
-    # Load data from tsv files in directory
-    train = read_data(input_filepath + train_file_name)
-    validation = read_data(input_filepath + validation_file_name)
-    test = pd.read_csv(input_filepath + test_file_name, sep='\t')
-
-    # Select columns to use
-    X_train, y_train = train['title'].values, train['tags'].values
-    X_val, y_val = validation['title'].values, validation['tags'].values
-    X_test = test['title'].values
-
+def bow_features(X_train, y_train, X_val, X_test):
     words_counts = count_words_strings(X_train)
     tags_counts = count_words_lists(y_train)
+
+    DICT_SIZE = 5000
+    INDEX_TO_WORDS = sorted(words_counts, key=words_counts.get, reverse=True)[:DICT_SIZE]
+    WORDS_TO_INDEX = {word: i for i, word in enumerate(INDEX_TO_WORDS)}
+    ALL_WORDS = WORDS_TO_INDEX.keys()
 
     X_train_mybag = sp_sparse.vstack(
         [sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_train])
@@ -105,6 +81,55 @@ def main(input_filepath='../../data/interim/', output_filepath='../../data/proce
         [sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_val])
     X_test_mybag = sp_sparse.vstack(
         [sp_sparse.csr_matrix(my_bag_of_words(text, WORDS_TO_INDEX, DICT_SIZE)) for text in X_test])
+
+    return X_train_mybag, X_val_mybag, X_test_mybag
+
+
+def main(input_filepath='../../data/interim/', output_filepath='../../data/processed/'):
+    """ Runs data processing scripts to turn pre-processed data from (../interim) into
+        feature data ready to be trained/tested (saved in ../processed).
+    """
+    logger = logging.getLogger(__name__)
+    logger.info('making final data set from interim data')
+
+    train_file_name_in = input_filepath + 'train.tsv'
+    val_file_name_in = input_filepath + 'validation.tsv'
+    test_file_name_in = input_filepath + 'test.tsv'
+
+    train_file_name_out = output_filepath + 'train.tsv'
+    val_file_name_out = output_filepath + 'validation.tsv'
+    test_file_name_out = output_filepath + 'test.tsv'
+
+    # Load data from tsv files in directory
+    train = read_data(train_file_name_in)
+    validation = read_data(val_file_name_in)
+    test = read_data(test_file_name_in)
+
+    logger.info(
+        'Finished reading data from: \n\t' + train_file_name_in + '\n\t' + val_file_name_in + '\n\t' + test_file_name_in)
+
+    # Select columns to use
+    X_train, y_train = train['title'].values, train['tags'].values
+    X_val, y_val = validation['title'].values, validation['tags'].values
+    X_test = test['title'].values
+
+    bow_train, bow_val, bow_test = bow_features(X_train, y_train, X_val, X_test)
+    logger.info('Finished generating the bag of words matrices')
+
+    tfidf_train, tfidf_val, tfidf_test, tfidf_vocab = tfidf_features(X_train, X_val, X_test)
+    logger.info('Finished generating the tfidf')
+
+    #  Lists to pd for easy writing
+    train_out = pd.DataFrame(list(zip(X_train, y_train, bow_train, tfidf_train)),
+                             columns=['title', 'tags', 'bow', 'tfidf'])
+    val_out = pd.DataFrame(list(zip(X_val, y_val, bow_val, tfidf_val)),
+                           columns=['title', 'tags', 'bow', 'tfidf'])
+    test_out = pd.DataFrame(list(zip(X_test, bow_test, tfidf_test)), columns=['title', 'bow', 'tfidf'])
+
+    # Write data to files
+    write_data(train_out, train_file_name_out, ';')
+    write_data(val_out, val_file_name_out, ';')
+    write_data(test_out, test_file_name_out, ';')
 
 
 if __name__ == '__main__':
--- src/util/util.py
+++ src/util/util.py
@@ -3,11 +3,12 @@ from ast import literal_eval
 import pandas as pd
 
 
-def read_data(filename):
-    data = pd.read_csv(filename, sep='\t')
-    data['tags'] = data['tags'].apply(literal_eval)
+def read_data(filename, delim='\t'):
+    data = pd.read_csv(filename, sep=delim)
+    if 'tags' in data.columns:
+        data['tags'] = data['tags'].apply(literal_eval)
     return data
 
-def write_data(data, filename):
-    data.to_csv(filename)
+def write_data(data, filename, delim='\t'):
+    data.to_csv(filename, sep=delim)
     return True
