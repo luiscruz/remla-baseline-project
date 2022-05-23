e734d5bcb24a66e5c78c5d7ddfe92181e978cdcf
ptemarvelde <45282601+ptemarvelde@users.noreply.github.com>
2022-05-23 16:23:39 +0200

    Include static checks and add Github Actions
    * started static checks
    
    * moved static checks to Makefile
    
    * Revert "moved static checks to Makefile"
    
    This reverts commit 9b946a747e2989fe47aad086ca8469f0b3dd3895.
    
    * move pre-commit script
    
    * run black
    
    * add github actions
    
    * edit makefile
    
    * update actions
    
    * update actions
    
    * update actions
    
    * testing actions
    
    * update reqs and always run checks
    
    * upload mllint report as artifact
    
    * upload mllint report as artifact
    
    * upload mllint report as artifact
    
    * always upload mllint report as artifact
    
    * always upload mllint report as artifact
    
    * always upload mllint report as artifact
    
    * try changing permissions
    
    * try changing permissions
    
    * always upload mllint report as artifact
    
    * include dockerifle and static-checks action job
    
    * sepearted action stages
    
    * test container build and push
    
    * revert splitting actions
    
    * change reponame to be lowercase
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * edit dockerfile
    
    * change python version
    
    * install make
    
    * edit Dockerfile
    
    * fix sudo not found
    
    * fix permissions
    
    * try workflow
    
    * comment out build/push workflow and remove Dockerfile
    
    * fix workflow file
    
    * rename workflow
    
    * add missing steps to test CI/CD job
    
    * try fix build fail

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -1,15 +1,17 @@
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.linear_model import LogisticRegression
+"""Module used to train the model in the ML pipeline"""
 import pickle
-import pandas as pd
-import yaml
 
-params = yaml.safe_load(open("params.yaml"))
+import yaml
+from sklearn.linear_model import LogisticRegression
+from sklearn.multiclass import OneVsRestClassifier
+
+with open("params.yaml", encoding="utf-8") as f:
+    params = yaml.safe_load(f)
 featurize_params = params["featurize"]
 train_params = params["train"]
 
-INPUT_TRAIN_PATH = featurize_params['output_train']
-OUT_PATH_MODEL = train_params['model_out']
+INPUT_TRAIN_PATH = featurize_params["output_train"]
+OUT_PATH_MODEL = train_params["model_out"]
 
 # def train_mybag(X_train, X_val, X_test, y_train):
 #     """
@@ -40,30 +42,28 @@ OUT_PATH_MODEL = train_params['model_out']
 
 """
 MultiLabel classifier
-In this task each example can have multiple tags therefore transform labels in a binary form and the prediction will be a mask of 0s and 1s. 
+In this task each example can have multiple tags therefore transform labels in a binary form
+and the prediction will be a mask of 0s and 1s.
 For this purpose it is convenient to use MultiLabelBinarizer from sklearn.
 """
 
 
-def train_classifier(X_train, y_train, penalty='l1', C=1):
+def train_classifier(X_train, y_train, penalty="l1", C=1):
     """
-      X_train, y_train — training data
-      
-      return: trained classifier
+    X_train, y_train — training data
+
+    return: trained classifier
     """
 
     # Create and fit LogisticRegression wrapped into OneVsRestClassifier.
-    clf = LogisticRegression(penalty=penalty, C=C, dual=False, solver='liblinear')
+    clf = LogisticRegression(penalty=penalty, C=C, dual=False, solver="liblinear")
     clf = OneVsRestClassifier(clf)
     clf.fit(X_train, y_train)
 
     return clf
 
 
-def train_classifier_for_transformations(
-    X_train_tfidf,
-    y_train
-):
+def train_classifier_for_transformations(X_train_tfidf, y_train):
     """
     :param X_train_tfidf:
     :param y_train:
@@ -72,20 +72,27 @@ def train_classifier_for_transformations(
     classifier_tfidf = train_classifier(X_train_tfidf, y_train)
     return classifier_tfidf
 
+
 def pickle_model(clf):
-    with open(OUT_PATH_MODEL, 'wb') as fd:
+    """Save model to disk using pickle"""
+    with open(OUT_PATH_MODEL, "wb") as fd:
         pickle.dump(clf, fd, protocol=pickle.HIGHEST_PROTOCOL)
 
+
 def load_pickled_train_data():
-    with open(INPUT_TRAIN_PATH, 'rb') as fd:
+    """Load train data from disk using pickle"""
+    with open(INPUT_TRAIN_PATH, "rb") as fd:
         X_train, y_train = pickle.load(fd)
     return X_train, y_train
 
+
 def main():
+    """Run train model steps"""
     X_train, y_train = load_pickled_train_data()
     clf = train_classifier_for_transformations(X_train, y_train)
 
     pickle_model(clf)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
