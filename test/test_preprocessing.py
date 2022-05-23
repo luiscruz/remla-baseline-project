1d6076d48a25365cec8bd008c32a508219d25908
Wander <wandersiemers@me.com>
2022-05-23 19:04:29 +0200

    Added test

---
+++ test/test_preprocessing.py
@@ -0,0 +1,13 @@
+import pandas as pd
+from src import preprocessing
+
+def test_read_file():
+    train = preprocessing.read_data('./data/train.tsv')
+    validation = preprocessing.read_data('data/validation.tsv')
+    test = pd.read_csv('data/test.tsv', sep='\t')
+
+    assert train.empty is False
+    assert validation.empty is False
+    assert test.empty is False
+
+
