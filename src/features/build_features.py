6c6522b794eaf2195dd57539401a24f5ad3440d4
Cathrine Paulsen <c.r.paulsen@student.tudelft.nl>
2022-05-23 15:36:57 +0200

    Fix dataframe dtypes

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -45,9 +45,9 @@ def tfidf_features(X_train_, X_val_, X_test_):
 
 if __name__ == '__main__':
     train = read_data(ROOT_DIR / 'data/raw/train.tsv',
-                      dtype={'title': 'string', 'tags': pd.arrays.StringArray})
+                      dtype={'title': 'string', 'tags': 'string'})
     validation = read_data(ROOT_DIR / 'data/raw/validation.tsv',
-                           dtype={'title': 'string', 'tags': pd.arrays.StringArray})
+                           dtype={'title': 'string', 'tags': 'string'})
     test = pd.read_csv(ROOT_DIR / 'data/raw/test.tsv', sep='\t', dtype={'title': 'string'})
 
     X_train, y_train = train['title'].values, train['tags'].values
