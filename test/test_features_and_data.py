1dd66b749075b87207b400e4d1ad4f797730c896
Jord Molhoek <j.molhoek@student.tudelft.nl>
2022-05-24 15:25:26 +0200

    Setup skeleton for testing

---
+++ test/test_features_and_data.py
@@ -0,0 +1,10 @@
+"""
+    ML Testing the StackOverflow label predictor for features and data. Making use of the [todo library name] library.
+"""
+import libtest.features_and_data
+
+
+def test_no_unsuitable_features():
+    libtest.features_and_data.no_unsuitable_features(['title'], [])
+
+# todo add more
---
+++ test/test_model_development.py
@@ -0,0 +1,7 @@
+"""
+    ML Testing the StackOverflow label predictor for model development. Making use of the [todo library name] library.
+"""
+import libtest.model_development
+
+
+# todo add more
