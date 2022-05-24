4a75db3c3db5ef46707d78f501016c9dce91cc60
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-05-24 17:09:42 +0200

    All tests are valid

--- tests/data_validation.py
+++ tests/data_validation.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from requests import get
 import tensorflow_data_validation as tfdv
 import pandas as pd
 import json
@@ -19,10 +20,30 @@ def read_info_anomaly(anomaly):
     return affected_area, problems
 
 
+def get_anomalies_detected(anomalies):
+    anomalies_detected = defaultdict(list)
+
+    for info in anomalies.anomaly_info.items():
+        affected_area, problems = read_info_anomaly(info)
+        anomalies_detected[affected_area].append(problems)
+
+    return anomalies_detected
+
+
+def dump_anomalies(anomalies_detected, option):
+    file_anomalies = f'../validation/{option}.json'
+    with open(file_anomalies, "w") as file_write:
+        file_write.write(json.dumps(anomalies_detected, indent=4))
+
+
 def detect_anomalies():
-    train_df = pd.read_csv(f'../data/train.tsv', sep="\t")
-    val_df = pd.read_csv(f'../data/validation.tsv', sep="\t")
-    test_df = pd.read_csv(f'../data/validation.tsv', sep="\t")
+    train_df = pd.read_csv(f'data/train.tsv', sep="\t")
+    val_df = pd.read_csv(f'data/validation.tsv', sep="\t")
+    test_df = pd.read_csv(f'data/validation.tsv', sep="\t")
+
+    valid_data = True
+
+    # For the validation set
 
     train_stats = tfdv.generate_statistics_from_dataframe(train_df)
     val_stats = tfdv.generate_statistics_from_dataframe(val_df)
@@ -30,17 +51,27 @@ def detect_anomalies():
     schema = tfdv.infer_schema(train_stats)
 
     anomalies = tfdv.validate_statistics(val_stats, schema=schema)
-    anomalies_detected = defaultdict(list)
 
-    for info in anomalies.anomaly_info.items():
-        affected_area, problems = read_info_anomaly(info)
-        anomalies_detected[affected_area].append(problems)
+    val_anomalies_detected = get_anomalies_detected(anomalies)
 
-    valid_data = len(anomalies_detected.keys()) == 0
+    if len(val_anomalies_detected.keys()) > 0:
+        valid_data = False
+        dump_anomalies(val_anomalies_detected)
 
-    if not valid_data:
-        # What do we do with this?
-        data_anomalies = json.dumps(anomalies_detected, indent=4)
-        print(data_anomalies)
+    # For the test set
+
+    train_df = train_df.drop(columns=['tags'])
+
+    train_stats = tfdv.generate_statistics_from_dataframe(train_df)
+    test_stats = tfdv.generate_statistics_from_dataframe(test_df)
+    anomalies = tfdv.validate_statistics(test_stats, schema=schema)
+
+    schema = tfdv.infer_schema(train_stats)
+
+    test_anomalies_detected = get_anomalies_detected(anomalies)
+
+    if len(test_anomalies_detected.keys()) > 0:
+        valid_data = False
+        dump_anomalies(test_anomalies_detected)
 
     return valid_data
