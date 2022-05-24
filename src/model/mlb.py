2f2d5b8ed7878beb5eafaa5741d8a2713ff7a8d8
Wander <wandersiemers@me.com>
2022-05-24 15:16:57 +0200

    Make binarizer and classifiers verbose

--- src/model/mlb.py
+++ src/model/mlb.py
@@ -3,7 +3,7 @@ from sklearn.multiclass import OneVsRestClassifier
 from sklearn.linear_model import LogisticRegression, RidgeClassifier
 
 def get_mlb(tags_counts, y_train, y_val):
-    mlb = MultiLabelBinarizer(classes=sorted(tags_counts.keys()))
+    mlb = MultiLabelBinarizer(classes=sorted(tags_counts.keys()), verbose=1)
     y_train = mlb.fit_transform(y_train)
     y_val = mlb.fit_transform(y_val)
 
@@ -20,8 +20,8 @@ def train_classifier(X_train, y_train, penalty='l1', C=1):
     # Create and fit LogisticRegression wraped into OneVsRestClassifier.
 
     clf = LogisticRegression(penalty=penalty, C=C,
-                             dual=False, solver='liblinear')
-    clf = OneVsRestClassifier(clf)
+                             dual=False, solver='liblinear', verbose=1)
+    clf = OneVsRestClassifier(clf, verbose=1)
     clf.fit(X_train, y_train)
 
     return clf
