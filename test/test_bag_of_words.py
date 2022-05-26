decb567ae5a25d43e354ddd117fe8bb33973a33b
Douwe den Blanken <douwedb@gmail.com>
2022-05-26 17:36:23 +0200

    Format tests

--- test/test_bag_of_words.py
+++ test/test_bag_of_words.py
@@ -1,12 +1,13 @@
 from src.model import bag_of_words
 
+
 def test_bag_of_words():
-    words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
-    examples = ['hi how are you']
+    words_to_index = {"hi": 0, "you": 1, "me": 2, "are": 3}
+    examples = ["hi how are you"]
     answers = [[1, 1, 0, 1]]
-    
+
     for ex, ans in zip(examples, answers):
         if (bag_of_words.bag_of_words(ex, words_to_index, 4) != ans).any():
             return f"Wrong answer for the case: '{ex}'"
 
-    return 'Basic tests are passed.'
+    return "Basic tests are passed."
--- test/test_preprocessing.py
+++ test/test_preprocessing.py
@@ -1,22 +1,30 @@
 import pandas as pd
 from src.model import preprocessing
 
+
 def test_read_file():
-    train = preprocessing.read_data('./data/train.tsv')
-    validation = preprocessing.read_data('data/validation.tsv')
-    test = pd.read_csv('data/test.tsv', sep='\t')
+    train = preprocessing.read_data("./data/train.tsv")
+    validation = preprocessing.read_data("data/validation.tsv")
+    test = pd.read_csv("data/test.tsv", sep="\t")
 
     assert train.empty is False
     assert validation.empty is False
     assert test.empty is False
 
-def test_text_prepare():
-	examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
-				"How to free c++ memory vector<int> * arr?"]
-	answers = ["sql server equivalent excels choose function", 
-			   "free c++ memory vectorint arr"]
-	for ex, ans in zip(examples, answers):
-		if preprocessing.text_prepare(ex) != ans:
-			return "Wrong answer for the case: '%s'" % ex
-	return 'Basic tests are passed.'
 
+def test_text_prepare():
+    examples = [
+        "SQL Server - any equivalent of Excel's CHOOSE function?",
+        "How to free c++ memory vector<int> * arr?",
+    ]
+
+    answers = [
+        "sql server equivalent excels choose function",
+        "free c++ memory vectorint arr",
+    ]
+
+    for ex, ans in zip(examples, answers):
+        if preprocessing.text_prepare(ex) != ans:
+            return f"Wrong answer for the case: '{ex}'"
+
+    return "Basic tests are passed."
