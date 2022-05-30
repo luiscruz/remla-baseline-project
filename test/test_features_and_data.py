a9f9ced5e281ba7465a5c54ca4ddf3c74c932f52
Jord Molhoek <j.molhoek@student.tudelft.nl>
2022-05-30 10:33:43 +0200

    WIP test pipeline failure if static analysis fails

--- test/test_features_and_data.py
+++ test/test_features_and_data.py
@@ -2,9 +2,10 @@
     ML Testing the StackOverflow label predictor for features and data. Making use of the [todo library name] library.
 """
 import libtest.features_and_data
+from src.text_preprocessing import feature_list
 
 
 def test_no_unsuitable_features():
-    libtest.features_and_data.no_unsuitable_features(['title', 'x'], ['x'])
+    libtest.features_and_data.no_unsuitable_features(feature_list, [])
 
 # todo add more
