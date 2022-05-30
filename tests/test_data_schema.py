ed95b6e6c32601d00613b8cecda5995d27edd3a7
Chiel de Vries <c.devries-1@student.tudelft.nl>
2022-05-30 20:06:51 +0200

    Add schema validation for interim and raw

--- tests/test_data_schema.py
+++ tests/test_data_schema.py
@@ -1,39 +1,34 @@
-import unittest
-import os.path as path
+import pickle
+import pytest
+import pandas as pd
 import tensorflow_data_validation as tfdv
 
 
 REFRESH_SCHEMAS = False
 
 
-class TestSchemas(unittest.TestCase):
+@pytest.mark.parametrize(
+    'data_step, data_set',
+    [('raw', 'test'),       ('raw', 'train'),       ('raw', 'validation'),
+     ('interim', 'test'),   ('interim', 'train'),   ('interim', 'validation')]
+)
+def test_schema(data_step, data_set):
 
-    def test_anomalies(self):
-        datasets = [
-            'train',
-            'test',
-            'validation',
-            'text_prepare_tests',
-        ]
-        for dataset in datasets:
-            with self.subTest(dataset=dataset):
-                # get paths to resources
-                data_path = '../data/{}.tsv'.format(dataset)
-                schema_path = 'schemas/{}_schema.pbtxt'.format(dataset)
+    data_path = '../data/{}/{}.tsv'.format(data_step, data_set)
+    schema_path = 'schemas/{}/{}_schema.pbtxt'.format(data_step, data_set)
 
-                # infer schema from data
-                stats = tfdv.generate_statistics_from_csv(data_path, delimiter='\t')
-                inferred_schema = tfdv.infer_schema(stats)
+    # infer schema from data
+    stats = tfdv.generate_statistics_from_csv(data_path, delimiter='\t')
+    inferred_schema = tfdv.infer_schema(stats)
 
-                # load schema from file
-                if REFRESH_SCHEMAS:
-                    tfdv.write_schema_text(inferred_schema, schema_path)
-                expected_schema = tfdv.load_schema_text(schema_path)
+    # load schema from file
+    if REFRESH_SCHEMAS:
+        tfdv.write_schema_text(inferred_schema, schema_path)
+    expected_schema = tfdv.load_schema_text(schema_path)
 
-                # ensure there are no anomalies
-                anomalies = tfdv.validate_statistics(stats, expected_schema)
-                self.assertEqual(0, len(anomalies.anomaly_info))
+    # ensure there are no anomalies
+    anomalies = tfdv.validate_statistics(stats, expected_schema)
+    assert 0 == len(anomalies.anomaly_info)
 
 
-if __name__ == '__main__':
-    unittest.main()
+
