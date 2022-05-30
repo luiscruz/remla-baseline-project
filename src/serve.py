4b9d5ea170cf0c16ea64233dd7a2c4dcb06b8599
AbeleMM <AbeleMM@users.noreply.github.com>
2022-05-30 18:20:36 +0200

    Remove extra printouts from serve script

--- src/serve.py
+++ src/serve.py
@@ -15,22 +15,15 @@ app = Flask(__name__)
 @app.route("/predict", methods=["POST"])
 def predict():
     """Predict the tags for a given question."""
-    print("owoo")
     input_data = request.get_json()
-    print(input_data)
     title = input_data.get("title")
-    print(title)
     processed_title_tfidf = TFIDF_VECTORIZER.transform([text_prepare(title)])
-    print(processed_title_tfidf)
     prediction = MLB.inverse_transform(MODEL.predict(processed_title_tfidf))[0]
-    print(prediction)
-
     res = {
         "result": prediction,
         # "classifier": "",
         "title": title,
     }
-    print(res)
     return jsonify(res)
 
 
