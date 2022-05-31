7a0e22d52bd52485e417d5aa4dad86de974048ab
tomasherq <T.HernandezQuintanilla@student.tudelft.nl>
2022-05-31 21:43:39 +0200

    Added mutamorphic to preprocessing and mut test

--- src/p1_preprocessing.py
+++ src/p1_preprocessing.py
@@ -3,17 +3,50 @@ import numpy as np
 import pandas as pd
 from ast import literal_eval
 from nltk.corpus import stopwords
-import sys
 import nltk
 import re
+import argparse
+import os
+import sys
 nltk.download('stopwords')
+sys.path.append(os.getcwd()+"/mutamorfic")
+sys.path.append(os.getcwd())
 
-# Do we need the validation really?
+
+def check_arguments():
+
+    parser = argparse.ArgumentParser(
+        description='Extra arguments can be specified for having a mutamorphic transformation.')
+
+    parser.add_argument("--replace", "-r", nargs=2, metavar=('<int>', '<str>'),
+                        help="This option activates transformation by replacement. Need to specify the following: <Nº of words to replace> <Type of replace (random/most common word)>", required=False)
+    parser.add_argument("--jumps", "-j",  type=int, required=False, help="Change every 1/jump words.")
+
+    args = parser.parse_args()
+
+    selected_options = {}
+
+    replace_valid = args.replace is not None and len(args.replace) > 1
+
+    if replace_valid:
+        if args.replace[1] in ["random", "most_common_first"] and args.replace[0].isdigit():
+
+            words_replace = int(args.replace[0])
+            if words_replace > 0:
+                selected_options["replace"] = {"n_words_replace": words_replace, "strategy": args.replace[1]}
+
+    selected_options["jumps"] = {"number": args.jumps, "counter": 0}
+    return selected_options
+
+
+selected_options = check_arguments()
 
 # RegEx expressions to clean the data
 REPLACE_BY_SPACE_RE = re.compile('[/(){}\[\]\|@,;]')
 BAD_SYMBOLS_RE = re.compile('[^0-9a-z #+_]')
 STOPWORDS = set(stopwords.words('english'))
+
+# The arguments for having mutamorphic stuff
 
 
 def read_data(filename):
@@ -24,11 +57,24 @@ def read_data(filename):
     return data
 
 
-def text_prepare(text):
+def text_prepare(text, mutator=None):
     """
         text: a string
         return: modified initial string
     """
+
+    # We could change this to be better
+    if mutator is not None:
+
+        if selected_options["jumps"]["counter"] == 0:
+            mutant_text = mutator.mutate(text, 36)
+            if len(mutant_text) > 0:
+                text = mutant_text[0]
+
+        if selected_options["jumps"]["number"] is not None:
+            selected_options["jumps"]["counter"] += 1
+            if selected_options["jumps"]["number"] <= selected_options["jumps"]["counter"]:
+                selected_options["jumps"]["counter"] = 0
 
     text = text.lower()  # lowercase text
     text = re.sub(REPLACE_BY_SPACE_RE, " ", text)  # replace REPLACE_BY_SPACE_RE symbols by space in text
@@ -38,6 +84,12 @@ def text_prepare(text):
 
 
 def get_preprocessed_data(path_data="data/"):
+
+    from mutamorfic.mutators import ReplacementMutator
+
+    if "replace" in selected_options:
+        mutator = ReplacementMutator(selected_options["replace"]["n_words_replace"],
+                                     1, selected_options["replace"]["strategy"])
 
     # Read the data to be used in the project
     train = read_data(f'{path_data}train.tsv')
@@ -50,9 +102,9 @@ def get_preprocessed_data(path_data="data/"):
     X_test = test['title'].values
 
     # Retrieve preprocesed data
-    X_train = [text_prepare(x) for x in X_train]
-    X_val = [text_prepare(x) for x in X_val]
-    X_test = [text_prepare(x) for x in X_test]
+    X_train = [text_prepare(x, mutator) for x in X_train]
+    X_val = [text_prepare(x, mutator) for x in X_val]
+    X_test = [text_prepare(x, mutator) for x in X_test]
 
     return {"X_train": X_train, "X_val": X_val, "X_test": X_test, "y_train": y_train, "y_val": y_val}
 
--- tests/mutamorphic_test.py
+++ tests/mutamorphic_test.py
@@ -1,11 +1,115 @@
-# Input phrases
+from joblib import load
+import json
+import random
+import sys
+import os
+from dependencies.model_functions import *
+import numpy as np
 
-# Function to change it with
-# synonyms
-# hypernyms
-# hyponyms
+sys.path.append(os.getcwd()+"/mutamorfic")
+sys.path.append(os.getcwd())
+
+SEED = 600
+random.seed(SEED)
+LIMIT = 2000
 
 
-# Obtain results for set of phrases
+def load_test_phrases(limit=LIMIT):
 
-# Compare the results
+    from src import p1_preprocessing
+
+    validation_data = p1_preprocessing.read_data("data/validation.tsv")
+
+    X_val, y_val = validation_data['title'].values, validation_data['tags'].values
+
+    lower_limit = random.randint(0, len(X_val)-limit-1)
+    upper_limit = lower_limit+limit
+
+    return X_val[lower_limit:upper_limit], y_val[lower_limit:upper_limit]
+
+
+def load_x_train():
+    from src import p1_preprocessing
+
+    return p1_preprocessing.read_data(f'data/train.tsv')['title'].values
+
+
+def change_phrases(X_val, num_replacements=2, num_variants=3, selection_strategy="random"):
+    from mutamorfic.mutators import ReplacementMutator
+
+    mutator = ReplacementMutator(num_replacements, num_variants, selection_strategy)
+
+    new_x_vals = []
+    for i in range(0, num_variants):
+        new_x_vals.append(list())
+
+    for sentence in X_val:
+        results = mutator.mutate(sentence, SEED)
+
+        for i in range(0, num_variants):
+
+            if len(results) == num_variants:
+                new_x_vals[i].append(results[i])
+            else:
+                # This way we ENSURE that both results have equal length
+                new_x_vals[i].append(sentence)
+
+    return new_x_vals
+    # To be implemented
+
+
+def get_values_classifiers(X_val, y_val, X_train):
+    from src import p1_preprocessing
+    from src import p2_text_processors
+    X_val = [p1_preprocessing.text_prepare(x) for x in X_val]
+
+    DICT_SIZE, INDEX_TO_WORDS = p2_text_processors.get_tags_and_words(X_val, y_val)[:2]
+
+    tfidf_vectorizer = p2_text_processors.TfidfVectorizer(min_df=5, max_df=0.9, ngram_range=(1, 2),
+                                                          token_pattern='(\S+)')
+
+    X_train = [p1_preprocessing.text_prepare(x) for x in X_train]
+
+    X_val_bag = p2_text_processors.get_x_values_bag(INDEX_TO_WORDS, DICT_SIZE, X_val)
+    tfidf_vectorizer.fit_transform(X_train)
+
+    X_val_tfidf = tfidf_vectorizer.transform(X_val)
+
+    return X_val_bag, X_val_tfidf
+
+
+def test_mutamorphic():
+
+    from src import p2_text_processors
+
+    X_val, y_val = load_test_phrases()
+    X_train = load_x_train()
+
+    classifiers = load("tests/dependencies/classifiers.joblib")
+    y_val = load('tests/dependencies/val_data.joblib')[:LIMIT]
+    bag_classifier = classifiers["bag"]
+    tfidf_classifier = classifiers["tfidf"]
+
+    # First we ge the unaltered labels
+
+    X_val_bag, X_val_tfidf = get_values_classifiers(X_val, y_val, X_train)
+
+    labels_bag_og = bag_classifier.predict(X_val_bag)
+    labels_tfidf_og = tfidf_classifier.predict(X_val_tfidf)
+
+    # Now we go for the mutants
+    X_val_mutates = change_phrases(X_val)
+
+    for X_val_mutate in X_val_mutates:
+        X_val_bag, X_val_tfidf = get_values_classifiers(np.array(X_val_mutate), y_val, X_train)
+
+        labels_bag = bag_classifier.predict(X_val_bag)
+        labels_tfidf = tfidf_classifier.predict(X_val_tfidf)
+
+        check_diff(get_diff_stats(labels_bag_og, labels_bag, y_val))
+        check_diff(get_diff_stats(labels_tfidf_og, labels_tfidf, y_val))
+
+    print("All tests passed.")
+
+
+test_mutamorphic()
