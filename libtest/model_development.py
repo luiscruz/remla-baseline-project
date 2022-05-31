29355c6401b3827205dd90b138a484758278117f
simran0413 <rrmsimran@gmail.com>
2022-05-31 14:54:37 +0200

    train baseline models

--- libtest/model_development.py
+++ libtest/model_development.py
@@ -5,7 +5,8 @@
     Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016).
     Available: https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf
 """
-
+from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LogisticRegression
 
 def compare_against_baseline(model = "linear", own_score,  train_X, train_Y, test_X, test_Y):
     options = ["linear", "logistic"]
@@ -13,6 +14,10 @@ def compare_against_baseline(model = "linear", own_score,  train_X, train_Y, tes
         model = "linear"
         
     # TRAINS Classifier
+    if model == "linear":
+        classifier = LinearRegression().fit(train_X, train_Y)
+    if model == "logistic":
+        classifier = LogisticRegression().fit(train_X, train_Y)
 
     # TESTS Classifier & RETURNS score
 
