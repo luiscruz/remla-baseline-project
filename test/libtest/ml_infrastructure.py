7d3fee5295e4a7fe1b9f34b09d3815b98d2501f6
Ziad <ziad.nawar7@yahoo.com>
2022-06-01 16:02:20 +0200

    add test_improved_model_quality in ml infrastructure

--- test/libtest/ml_infrastructure.py
+++ test/libtest/ml_infrastructure.py
@@ -7,22 +7,31 @@
 """
 from math import isclose
 
-from sklearn.metrics import f1_score, average_precision_score, accuracy_score, roc_auc_score
+from sklearn.metrics import f1_score, average_precision_score, accuracy_score
 
 
 def test_reproducibility_training(model, x_train, y_train, x_test, y_test, eval_metrics=None, precision=1e-3):
     models = {}
     for i in range(2):
         trained_model = model.fit(x_train, y_train)
-        y_labels = trained_model.predict(x_test)
-        y_score = trained_model.decision_function(x_test)
-        models["model_" + str(i)] = [f1_score(y_test, y_labels, average='weighted'),
-                                     average_precision_score(y_test, y_labels, average='macro'),
-                                     accuracy_score(y_test, y_labels),
-                                     roc_auc_score(y_test, y_score, multi_class='ovo')]
+
+        models["model_" + str(i)] = get_accuracy(trained_model, x_test, y_test)
 
     lst = all(isclose(x, y, rel_tol=precision) for x, y in zip(models["model_0"], models["model_1"]))
     print(lst, models)
     assert lst
+
+def test_improved_model_quality(model_1, model_2, x_test, y_test):
+    res1 = get_accuracy(model_1, x_test, y_test)
+    res2 = get_accuracy(model_2, x_test, y_test)
+    lst = all(y >= x for x, y in zip(res1, res2))
+    assert lst
+
+
+def get_accuracy(trained_model, x_test, y_test):
+    y_labels = trained_model.predict(x_test)
+    return [f1_score(y_test, y_labels, average='weighted'),
+            average_precision_score(y_test, y_labels, average='macro'),
+            accuracy_score(y_test, y_labels)]
 
 # todo add more
