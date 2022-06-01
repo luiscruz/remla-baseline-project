2daefa4b4485215aafe953da8cd856aad59db2b2
Ziad <ziad.nawar7@yahoo.com>
2022-06-01 15:39:00 +0200

    add test_reproducibility_training in ml infrastructure

--- test/libtest/ml_infrastructure.py
+++ test/libtest/ml_infrastructure.py
@@ -5,6 +5,24 @@
     Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016).
     Available: https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf
 """
+from math import isclose
 
+from sklearn.metrics import f1_score, average_precision_score, accuracy_score, roc_auc_score
+
+
+def test_reproducibility_training(model, x_train, y_train, x_test, y_test, eval_metrics=None, precision=1e-3):
+    models = {}
+    for i in range(2):
+        trained_model = model.fit(x_train, y_train)
+        y_labels = trained_model.predict(x_test)
+        y_score = trained_model.decision_function(x_test)
+        models["model_" + str(i)] = [f1_score(y_test, y_labels, average='weighted'),
+                                     average_precision_score(y_test, y_labels, average='macro'),
+                                     accuracy_score(y_test, y_labels),
+                                     roc_auc_score(y_test, y_score, multi_class='ovo')]
+
+    lst = all(isclose(x, y, rel_tol=precision) for x, y in zip(models["model_0"], models["model_1"]))
+    print(lst, models)
+    assert lst
 
 # todo add more
--- test/test_ml_infrastructure.py
+++ test/test_ml_infrastructure.py
@@ -1,7 +1,31 @@
 """
     ML Testing the StackOverflow label predictor for the ML infrastructure. Making use of the [todo library name] library.
 """
-import libtest.ml_infrastructure
+from math import isclose
 
+import joblib
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import roc_auc_score, f1_score, average_precision_score, accuracy_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
+
+from libtest.ml_infrastructure import test_reproducibility_training
+from model_training import train_classifier
+
+
+def test_reproducibility_training_specific():
+    print("Reproducibility Training example")
+    X_train_mybag, X_train_tfidf, X_val_mybag, X_val_tfidf = joblib.load("../output/vectorized_x.joblib")
+    y_train, y_val = joblib.load("../output/y_preprocessed.joblib")
+    tags_counts = joblib.load("../output/tags_counts.joblib")
+
+    mlb = MultiLabelBinarizer(classes=sorted(tags_counts.keys()))
+    y_train = mlb.fit_transform(y_train)
+    y_val = mlb.fit_transform(y_val)
+
+    clf = LogisticRegression(penalty='l1', C=1, dual=False, solver='liblinear')
+    clf = OneVsRestClassifier(clf)
+
+    test_reproducibility_training(clf, X_train_mybag, y_train, X_val_mybag, y_val)
 
 # todo add more
