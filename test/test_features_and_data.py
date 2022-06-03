d6ffb487efe2db9bab2c9d324c763ea3379cc1e6
Jord Molhoek <j.molhoek@student.tudelft.nl>
2022-06-03 09:26:02 +0200

    Fix correlation tests. Add preprocessing tests.

--- test/test_features_and_data.py
+++ test/test_features_and_data.py
@@ -4,11 +4,13 @@
 import joblib
 import numpy as np
 
-from libtest.features_and_data import no_unsuitable_features, pairwise_feature_correlations, feature_target_correlations
+import libtest.features_and_data as lib
+from src.text_preprocessing import text_prepare
+from src.vectorization import my_bag_of_words
 
 
 def test_no_unsuitable_features():
-    no_unsuitable_features(['title'], [])
+    lib.no_unsuitable_features(['title'], [])
 
 
 def prepare_correlation_analysis():
@@ -40,16 +42,16 @@ def prepare_correlation_analysis():
 def test_pairwise_feature_correlations():
     mybag, tfidf, _ = prepare_correlation_analysis()
 
-    pairwise_feature_correlations(mybag, sample_size=100000)
-    pairwise_feature_correlations(tfidf, sample_size=100000)
+    lib.pairwise_feature_correlations(mybag, sample_size=100000)
+    lib.pairwise_feature_correlations(tfidf, sample_size=100000)
 
 
 def test_feature_target_correlations():
     mybag, tfidf, labels_matrix = prepare_correlation_analysis()
 
     for i in range(3):
-        feature_target_correlations(mybag, labels_matrix[:, i])
-        feature_target_correlations(tfidf, labels_matrix[:, i])
+        lib.feature_target_correlations(mybag, labels_matrix[:, i])
+        lib.feature_target_correlations(tfidf, labels_matrix[:, i])
 
 
 def test_feature_values():
@@ -69,5 +71,25 @@ def test_feature_values():
         for k in features_values[i]:
             assert k in expected
         break
+
+
+def test_preprocessing_prepare():
+    examples = ["SQL Server - any equivalent of Excel's CHOOSE function?",
+                "How to free c++ memory vector<int> * arr?"]
+    answers = ["sql server equivalent excels choose function",
+               "free c++ memory vectorint arr"]
+
+    lib.preprocessing_validation(examples, answers, text_prepare)
+
+
+def test_preprocessing_bag_of_words():
+    words_to_index = {'hi': 0, 'you': 1, 'me': 2, 'are': 3}
+    examples = ['hi how are you']
+    answers = [[1, 1, 0, 1]]
+
+    lib.preprocessing_validation(examples, answers,
+                                 lambda x: my_bag_of_words(x, words_to_index, 4),
+                                 equals=lambda a, b: (a == b).all())
+
 
 # todo add more
