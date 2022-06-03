5d5c70b8cd83b0cfeeeb0ba46c7d9bc5f34a9866
nienkenijkamp <n.nijkamp@student.tudelft.nl>
2022-06-03 14:41:14 +0200

    Fix pylint errors

--- src/models/serve_model.py
+++ src/models/serve_model.py
@@ -3,9 +3,9 @@ Flask API of the SMS Spam detection model model.
 """
 
 import pickle
+import random
 from flask import Flask, jsonify, request, Response
 from flasgger import Swagger
-import random
 
 from src.config.definitions import ROOT_DIR
 from src.features.build_features import text_prepare
@@ -13,7 +13,7 @@ from src.features.build_features import text_prepare
 app = Flask(__name__)
 swagger = Swagger(app)
 
-num_pred = 0
+NUM_PRED = 0
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -46,15 +46,16 @@ def predict():
     prepared_title = text_prepare(title) # remove bad symbols
     processed_title = tfidf_vectorizer.transform([prepared_title])
 
-    with open(ROOT_DIR / 'models/tfidf.pkl', 'rb') as f:
-    	model = pickle.load(f)
+    with open(ROOT_DIR / 'models/tfidf.pkl', 'rb') as file:
+        model = pickle.load(file)
     prediction = model.predict(processed_title)[0]
-
-    global num_pred
-    num_pred = num_pred + 1  # Increment number of total predictions made
+    # global statement is used to keep track of predictions, this is the simplest solution
+    # pylint: disable=global-statement
+    global NUM_PRED
+    NUM_PRED = NUM_PRED + 1  # Increment number of total predictions made
 
     # TODO: Convert preediction: binary array -> list of tags as strings ?
-    
+
     return jsonify({
         "result": prediction.tolist(),
         "classifier": "tfifd multi-label-binarizer ",
@@ -74,7 +75,7 @@ def metrics():
 
     string += "# HELP num_pred Number of total predictions made\n"
     string += "# TYPE num_pred counter\n"
-    string += "num_pred " + str(num_pred) + "\n\n"
+    string += "num_pred " + str(NUM_PRED) + "\n\n"
 
     # Note: Prometheus requires mimetype to be explicitly set to text/plain
     return Response(string, mimetype='text/plain')
