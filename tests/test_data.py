f63a2b6de41efbf7dade7222f34880a408c59aa1
moudemans <melchior.oudemans@gmail.com>
2022-06-08 10:13:51 +0200

    Added tests for the interim datasets

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,74 +1,66 @@
+import os
+
 import pandas
+import pandas as pd
 import pytest
 from src.util import util
 
+from os import listdir
+from os.path import isfile, join
 
-@pytest.fixture()
-def df1():
-    df1 = util.read_data('data/interim/test.tsv')
-    yield df1
-
-
-@pytest.fixture()
-def df2():
-    df2 = util.read_data('data/interim/train.tsv')
-    yield df2
+from util.util import read_data
 
 
 @pytest.fixture()
-def df3():
-    df3 = util.read_data('data/interim/validation.tsv')
-    yield df3
+def all_data():
+    all_data = []
+    input_filepath = '../data/interim/'
+    directory = os.getcwd()
+
+    print('########', directory)
+    onlyfiles = [join(input_filepath, f) for f in listdir(input_filepath) if isfile(join(input_filepath, f))]
+
+    for f in onlyfiles:
+        if ".tsv" not in f:
+            continue
+        file_data = read_data(f)
+        all_data.append(file_data)
+
+    yield all_data
 
 
-@pytest.fixture()
-def df_cross(df1, df2, df3):
-    df1['tags'] = df1['title'].apply(lambda _ : [])
-    df_cross = df1.append(df2).append(df3)
-    yield df_cross
+def test_no_duplicates(all_data):
+    for df in all_data:
+        if "tags" not in df.columns:
+            df["tags"] = " "
+        df['key'] = df['title'] + df['tags'].apply(lambda r: str(r))
+        print('ROW: ', df.groupby(['key']).size().sort_values())
+        assert df.groupby(['key']).size().max() == 1
 
 
-def test_no_duplicates1(df1):
-    assert df1.groupby(['title']).size().max() == 1
 
 
-def test_no_duplicates2(df2):
-    df2['key'] = df2['title'] + df2['tags'].apply(lambda r: str(r))
+def test_no_duplicates_cross_dataset(all_data):
+    df_cross = pd.DataFrame(columns=['title', 'tags'])
+    for df in all_data:
+        if "tags" not in df.columns:
+            df["tags"] = " "
+        df_cross = pd.concat([df_cross, df])
 
-    assert df2.groupby(['key']).size().max() == 1
-
-
-def test_no_duplicates3(df3):
-    df3['key'] = df3['title'] + df3['tags'].apply(lambda r: str(r))
-    assert df3.groupby(['key']).size().max() == 1
-
-
-def test_no_duplicates_cross_dataset(df_cross):
     df_cross['key'] = df_cross['title'] + df_cross['tags'].apply(lambda r: str(r))
     print('ROW: ', df_cross.groupby(['key']).size().sort_values())
     assert df_cross.groupby(['key']).size().max() == 1
 
 
-def test_no_empty_tag1(df2):
-    df2['len'] = df2['tags'].map(len)
-    assert df2['len'].min() >= 1
+def test_no_empty_tag(all_data):
+    for df in all_data:
+        if 'tags' not in df.columns:
+            continue
+        df['len'] = df['tags'].map(len)
+        assert df['len'].min() >= 1
 
 
-def test_no_empty_tag2(df3):
-    df3['len'] = df3['tags'].map(len)
-    assert df3['len'].min() >= 1
-
-
-def test_no_empty_text1(df1):
-    df1['len'] = df1['title'].map(len)
-    assert df1['len'].min() >= 1
-
-
-def test_no_empty_text2(df2):
-    df2['len'] = df2['title'].map(len)
-    assert df2['len'].min() >= 1
-
-
-def test_no_empty_text3(df3):
-    df3['len'] = df3['title'].map(len)
-    assert df3['len'].min() >= 1
+def test_no_empty_text(all_data):
+    for df in all_data:
+        df['len'] = df['title'].map(len)
+        assert df['len'].min() >= 1
