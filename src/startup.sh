e95a77d003859bbd1a2b303b81bad47126d8b07d
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-06-09 18:10:35 +0200

    some changes

--- src/startup.sh
+++ src/startup.sh
@@ -11,22 +11,27 @@ echo "Current dir: $(pwd)"
 sleep 20
 
 # copy dvc-cache folder into root folder of this project
-echo "Copying $DVC_SHARED_ROOT_PROJECT/dvc-cache/ into ../"
-cp -rl $DVC_SHARED_ROOT_PROJECT/dvc-cache/ ../ 
+# echo "Copying $DVC_SHARED_ROOT_PROJECT/dvc-cache/ into current pwd"
+# cp -rl $DVC_SHARED_ROOT_PROJECT/dvc-cache/ .
 
-echo "Copying $DVC_SHARED_ROOT_PROJECT/dvc.lock into ../"
-cp $DVC_SHARED_ROOT_PROJECT/dvc.lock ../ 
+# echo "Copying $DVC_SHARED_ROOT_PROJECT/dvc.lock into current pwd"
+# cp $DVC_SHARED_ROOT_PROJECT/dvc.lock .
 
-echo "Setting dvc cache dir to ../dvc-cache"
+echo "Setting dvc cache dir to dvc-cache"
 
 # assign cache dir to the copied local cache
 # this is done so that when dvc-cache is updated using dvc repro
 # the dvc-cache in shared root project folder is not modified
 # since only continous training service should do that
 # inference service only fetches the info of the cache and data and reproduces them
-dvc cache dir ../dvc-cache
+dvc cache dir  $DVC_SHARED_ROOT_PROJECT/dvc-cache/
 
-echo "dvc cache dir set to ../dvc-cache"
+echo "Cache dir set to dvc-cache"
+
+# this is inefficient, but we need the files to be copied so that any changes to cache does not effect the mounted cache
+dvc config cache.type symlink 
+dvc config cache.shared group
+dvc unprotect $DVC_SHARED_ROOT_PROJECT/dvc-cache/
 
 # dvc config cache.shared group
 # dvc config cache.type symlink
