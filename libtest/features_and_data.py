cb3215c2f25e8bf6e485921a42a2a1b4aa174711
Mirijam <s.x.zhang@student.tudelft.nl>
2022-06-12 23:19:59 +0200

    Fix styleguide suggestions

--- libtest/features_and_data.py
+++ libtest/features_and_data.py
@@ -2,9 +2,9 @@
     ML Test library functions for features and data.
     Based on section 2 of the paper referenced below.
 
-    Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016).
-    Available: https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf
-"""
+    Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for
+    ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016). Available:
+    https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf """
 import collections
 import numpy as np
 
--- libtest/model_development.py
+++ libtest/model_development.py
@@ -2,9 +2,9 @@
     ML Test library functions for model development.
     Based on section 3 of the paper referenced below.
 
-    Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016).
-    Available: https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf
-"""
+    Eric Breck, Shanqing Cai, Eric Nielsen, Michael Salib, D. Sculley (2016). What’s your ML test score? A rubric for
+    ML production systems. Reliable Machine Learning in the Wild - NIPS 2016 Workshop (2016). Available:
+    https://storage.googleapis.com/pub-tools-public-publication-data/pdf/45742.pdf """
 from sklearn.dummy import DummyClassifier
 from sklearn.linear_model import LinearRegression
 from sklearn.linear_model import LogisticRegression
