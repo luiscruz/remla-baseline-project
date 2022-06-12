c1cf742a069a0738ffbfa8353c2363a75edd3d04
Pepijn te Marvelde <p.h.temarvelde@student.tudelft.nl>
2022-06-12 10:24:33 +0200

    remove image loading from minikube startup as containers are pulled from ghcr.io now.

--- deployment/minikube-build-and-push-containers.sh
+++ deployment/minikube-build-and-push-containers.sh
@@ -13,14 +13,14 @@ echo "$SHARED_FOLDER_HOSTPATH:$MOUNTING_PERSISTENT_STORAGE_MINIKUBE"
 
 minikube status || minikube start --cpus 4 --memory 8192
 
-eval $(minikube docker-env --shell bash)
-#eval $(minikube docker-env)
-
-# NOTE: use docker-compose to create the images beforehand, since minikube uses those images for deployment
-minikube image load inference-service:latest
-minikube image load scraping-service:latest
-minikube image load scraping-controller:latest
-minikube image load training-service:latest
+#eval $(minikube docker-env --shell bash)
+##eval $(minikube docker-env)
+#
+## NOTE: use docker-compose to create the images beforehand, since minikube uses those images for deployment
+#minikube image load inference-service:latest
+#minikube image load scraping-service:latest
+#minikube image load scraping-controller:latest
+#minikube image load training-service:latest
 
 #docker-compose -f docker-compose/docker-compose.yml build --no-cache
 
