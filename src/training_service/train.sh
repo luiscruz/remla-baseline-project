a040b7ef4e28e104e3627756fb7973d2af56045a
M-R-T-U-D <M.Rashad@student.tudelft.nl>
2022-06-14 00:12:12 +0200

    see whether the fix works for tracking/training again

--- src/training_service/train.sh
+++ src/training_service/train.sh
@@ -7,7 +7,7 @@ mkdir -p data/processed
 mkdir -p data/interim
 mkdir -p data/external
 
-dvc add $SHARED_DATA_PATH/raw/ -o data/raw/
+dvc add $SHARED_DATA_PATH/raw/ -o data/
 # dvc add $SHARED_DATA_PATH/raw/test.tsv -o data/raw/test.tsv
 # dvc add $SHARED_DATA_PATH/raw/train.tsv -o data/raw/train.tsv
 # dvc add $SHARED_DATA_PATH/raw/validation.tsv -o data/raw/validation.tsv
