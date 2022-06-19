a66c523df528c0d627370e766b87aef87e0e8d7a
Hielke Walinga <hielkewalinga@gmail.com>
2022-06-19 23:42:09 +0200

    refactored metrics and implemented end point

--- src/models/predict_model.py
+++ src/models/predict_model.py
@@ -1,34 +1,29 @@
 import logging
 import pickle
+from pathlib import Path
+from typing import Any, Dict, List, Optional
 
-from sklearn.metrics import (accuracy_score, average_precision_score, f1_score,
-                             recall_score)
+from sklearn.metrics import (
+    accuracy_score,
+    average_precision_score,
+    f1_score,
+    recall_score,
+)
 from sklearn.metrics import roc_auc_score as roc_auc
 
+from ..project_types import ModelName
 
-def print_evaluation_scores(logger, y_val, predicted_labels, predicted_scores):
-    logger.info(
-        "Accuracy score: " + "{:.2f}".format(accuracy_score(y_val, predicted_labels))
-    )
-    logger.info(
-        "Recall score: "
-        + "{:.2f}".format(recall_score(y_val, predicted_labels, average="macro"))
-    )
-    logger.info(
-        "F1 score: "
-        + "{:.2f}".format(f1_score(y_val, predicted_labels, average="weighted"))
-    )
-    logger.info(
-        "Average precision score: "
-        + "{:.2f}".format(
-            average_precision_score(y_val, predicted_labels, average="macro")
-        )
-    )
-    logger.info(
-        "ROC score: "
-        + "{:.2f}".format(roc_auc(y_val, predicted_scores, multi_class="ovo"))
-    )
-    logger.info("-------- \n")
+project_root = Path("../../")
+
+
+def validation_file(model: ModelName):
+    input_filepath = project_root / "data/processed/"
+    return pickle.load(input_filepath.joinpath(f"{model}_val.pickle").open("rb"))
+
+
+def model_file(model: ModelName):
+    model_filepath = project_root / "models/"
+    return pickle.load(model_filepath.joinpath(f"{model}").open("rb"))
 
 
 def main():
@@ -36,30 +31,62 @@ def main():
     logger.info("Starting the program")
 
     logger.info("Load data")
-    input_filepath = "../../data/processed/"
 
-    bow_val = pickle.load(open(input_filepath + "bow_val.pickle", "rb"))
-    tfidf_val = pickle.load(open(input_filepath + "tfidf_val.pickle", "rb"))
-    mlb_y_val = pickle.load(open(input_filepath + "mlb_val.pickle", "rb"))
-
-    model_filepath = "../../models/"
-    classifier_bow = pickle.load(open(model_filepath + "bow_model.pickle", "rb"))
-    classifier_tfidf = pickle.load(open(model_filepath + "tfidf_model.pickle", "rb"))
-
-    y_val_predicted_labels_bow = classifier_bow.predict(bow_val)
-    y_val_predicted_scores_bow = classifier_bow.decision_function(bow_val)
-
-    y_val_predicted_labels_tfidf = classifier_tfidf.predict(tfidf_val)
-    y_val_predicted_scores_tfidf = classifier_tfidf.decision_function(tfidf_val)
+    evaluator = Evaluator()
 
     logger.info("*** Bag-of-words scores ***")
-    print_evaluation_scores(
-        logger, mlb_y_val, y_val_predicted_labels_bow, y_val_predicted_scores_bow
-    )
+    print_evaluation_scores(logger, evaluator.evaluate(ModelName.bow))
     logger.info("*** Tfidf scores ***")
-    print_evaluation_scores(
-        logger, mlb_y_val, y_val_predicted_labels_tfidf, y_val_predicted_scores_tfidf
-    )
+    print_evaluation_scores(logger, evaluator.evaluate(ModelName.tfidf))
+
+
+class Evaluator:
+    def __init__(self):
+        self.mlb_y_val = validation_file(ModelName.mlb)
+
+    def evaluate(self, model: ModelName) -> Dict[str, float]:
+        model_val = validation_file(model)
+        classifier_model = model_file(model)
+
+        y_val_predicted_labels = classifier_model.predict(model_val)
+        y_val_predicted_scores = classifier_model.decision_function(model_val)
+
+        return {
+            "accuracy": accuracy_score(self.mlb_y_val, y_val_predicted_labels),
+            "recall": recall_score(
+                self.mlb_y_val, y_val_predicted_labels, average="macro"
+            ),
+            "f1-score": f1_score(
+                self.mlb_y_val, y_val_predicted_labels, average="weighted"
+            ),
+            "average-precision-score": average_precision_score(
+                self.mlb_y_val, y_val_predicted_labels, average="macro"
+            ),
+            "roc-score": roc_auc(
+                self.mlb_y_val, y_val_predicted_scores, multi_class="ovo"
+            ),
+        }
+
+
+def print_evaluation_scores(
+    logger: logging.Logger, scores: Dict[str, float], format: str
+):
+    text = evaluation_scores_to_text(scores, format)
+    for line in text:
+        logger.info()
+
+
+def evaluation_scores_to_text(
+    scores: Dict[str, float], format: str = "{:.2f}"
+) -> List[str]:
+    return [
+        "Accuracy score: " + format.format(scores["accuracy"]),
+        "Recall score: " + format.format(scores["recall"]),
+        "F1 score: " + format.format(scores["f1-score"]),
+        "Average precision score: " + format.format(scores["average-precision-score"]),
+        "ROC score: " + format.format(scores["roc-score"]),
+        "-------- \n",
+    ]
 
 
 if __name__ == "__main__":
--- src/serve_model.py
+++ src/serve_model.py
@@ -5,30 +5,45 @@
 """
 
 import os
+
 import uvicorn
 from fastapi import FastAPI
+
+from .models.predict_model import Evaluator
+from .project_types import ModelName
 
 app = FastAPI()
 
 
 @app.get("/")
-def hello_world():
+def root():
     return "<p>Hello, World!</p>"
 
 
-# TODO: Add a predict endpoint...
+@app.get("/metrics/")
+def all_metrics():
+    evaluator = Evaluator()
+    res = {}
+    for model_name in [ModelName.bow, ModelName.tfidf]:
+        res[model_name] = evaluator.evaluate(model_name)
+    return res
 
-@app.get("/metrics")
-def metrics():
-    text = "# HELP my_random A random number\n"
-    text += "# TYPE my_random gauge\n"
-    text += f"my_random {random.random()}\n\n"
 
-    return Response(text, mimetype='text/plain')
+@app.get("/metrics/{model_name}")
+def metrics(model_name: ModelName):
+    evaluator = Evaluator()
+    return {model_name: evaluator.evaluate(model_name)}
 
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    print(f"Running Flask app on port {port}")
+    print(f"Running FastAPI app on port {port}")
 
-    uvicorn.run("serve_model:app", host="0.0.0.0", port=port, debug=True, port=port, log_level='debug')
+    uvicorn.run(
+        "serve_model:app",
+        host="0.0.0.0",
+        port=port,
+        debug=True,
+        port=port,
+        log_level="debug",
+    )
